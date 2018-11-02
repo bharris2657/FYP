3a6db213cbe4dbddd1eff1e764ee593d4675a1d2
bharris2657 <up735437@myport.ac.uk>
2018-11-02 09:34:32 +0000

    Revert "Revert "Added arrayList of cards""
    This reverts commit 0a8ee24485db199111e77373b24ab7bfd6b23c0b.

--- app/src/main/java/com/example/benha/fyp/Review.java
+++ app/src/main/java/com/example/benha/fyp/Review.java
@@ -1,16 +1,85 @@
 package com.example.benha.fyp;
 
+import android.Manifest;
+import android.content.Context;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Review extends Fragment {
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Review extends Fragment implements View.OnClickListener {
+
+    private ArrayList<String> flashCards = new ArrayList<String>();
+
+    public Review(){}
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                          Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_review, container, false);
+        View thisView = inflater.inflate(R.layout.activity_review, container, false);
+        Button startReview = (Button) thisView.findViewById(R.id.startReview);
+        startReview.setOnClickListener(this);
+
+        return thisView;
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+    }
+
+    @Override
+    public void onClick(View v){
+        Button startReview = (Button) v.findViewById(R.id.startReview);
+        startReview.setVisibility(View.GONE);
+        populateArrayList();
+    }
+
+    public ArrayList<String[]> populateArrayList() {
+
+        ArrayList<String[]> flashCards = new ArrayList<String[]>();
+        //loads text database
+        File file = new File(Environment.getExternalStorageDirectory(),"cardDatabase.txt");
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                //creates arraylist of arrays referenced by 0 0 for question or 0 1 for answer 0 2 for score etc
+                    String[]lineArray = line.split(";");
+                    flashCards.add(lineArray);
+                    }
+
+                    br.close();
+            }
+            catch (IOException e) {
+
+            }
+
+        TextView textView = (TextView) getView().findViewById(R.id.testArray);
+        return flashCards;
+    }
+
 }
